56c3523e025da273b660dd39e3336cc7d7212802
LooanCheong <jjyejun@naver.com>
2023-04-04 00:23:45 +0900

    Music 모델 수정

--- src/models/Music.js
+++ src/models/Music.js
@@ -5,12 +5,9 @@ const songSchema = new mongoose.Schema({
   fileUrl: { type: String, required: true },
   thumbUrl: { type: String, required: true },
   description: { type: String, trim: true },
-  meta: {
-    views: { type: Number, default: 0, required: true },
-    like: { type: Number, default: 0, required: true },
-  },
+  views: { type: Number, default: 0, required: true },
+  like: { type: Number, default: 0, required: true },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 const Song = mongoose.model("Song", songSchema);
