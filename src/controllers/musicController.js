7d54a4ff1d256c40e9ca000c1d6bb21a7a90d594
LooanCheong <jjyejun@naver.com>
2023-04-04 00:59:26 +0900

    home, watch 함수 생성

--- src/controllers/musicController.js
+++ src/controllers/musicController.js
@@ -1,6 +1,14 @@
-import Song from "../models/Music";
+import Music from "../models/Music";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  return res.render("home", { pageTitle: "Home" });
+  const musics = await Music.find({}).sort({ like: "desc" });
+  console.log(musics);
+  return res.render("home", { pageTitle: "Home", musics });
+};
+
+export const watch = async (req, res) => {
+  const { id } = req.params;
+
+  return res.render("watch", { pageTitle: "watch" });
 };
